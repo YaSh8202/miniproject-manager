80d964b3f1a4d0e594b5902b3678819bac8ac50d
Yash Bajaj <bajajy82@gmail.com>
2023-10-26 02:40:42 +0530

    fixed ts build errors

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,8 @@
-import Link from "next/link";
-
-import { getServerAuthSession } from "@/server/auth";
 import Navbar from "./_components/navbar";
-// import { api } from "@/trpc/server";
 
-export default async function Home() {
+export default function Home() {
   // const hello = await api.post.hello.query({ text: "from tRPC" });
-  const session = await getServerAuthSession();
+  
 
   return (
     <main className="flex min-h-screen flex-col  ">
---
+++ src/app/coordinator/new-miniproject/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewMiniProject = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewMiniProject
